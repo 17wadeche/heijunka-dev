e3e263b0d10d8e60157ba541480b7cb694886e84
Chey Bronk-Wade <chey.wade@medtronic.com>
2025-12-19 14:27:23 -0600

    fix

--- run_all.py
+++ run_all.py
@@ -7,15 +7,15 @@ commands = [
     [PYTHON_BIN, "get_timeliness.py"],
     [PYTHON_BIN, "collect_metrics_dev.py", "--team", "ECT"],
     [PYTHON_BIN, "heijunka_new_layout.py", "--all"],
-    [PYTHON_BIN, "collect_non_wip_new.py --config teams.json --metrics metrics.csv --all --out non_wip.csv"],
-    [PYTHON_BIN, "push_selected_dates.py --date 2025-10-27"],
-    [PYTHON_BIN, "push_selected_dates.py --date 2025-11-03"],
-    [PYTHON_BIN, "push_selected_dates.py --date 2025-11-10"],
-    [PYTHON_BIN, "push_selected_dates.py --date 2025-11-17"],
-    [PYTHON_BIN, "push_selected_dates.py --date 2025-11-24"],
-    [PYTHON_BIN, "push_selected_dates.py --date 2025-12-01"],
-    [PYTHON_BIN, "push_selected_dates.py --date 2025-12-08"],
-    [PYTHON_BIN, "push_selected_dates.py --date 2025-12-15"]
+    [PYTHON_BIN, "collect_non_wip_new.py", "--config", "teams.json", "--metrics", "metrics.csv", "--all", "--out", "non_wip.csv"],
+    [PYTHON_BIN, "push_selected_dates.py", "--date", "2025-10-27"],
+    [PYTHON_BIN, "push_selected_dates.py", "--date", "2025-11-03"],
+    [PYTHON_BIN, "push_selected_dates.py", "--date", "2025-11-10"],
+    [PYTHON_BIN, "push_selected_dates.py", "--date", "2025-11-17"],
+    [PYTHON_BIN, "push_selected_dates.py", "--date", "2025-11-24"],
+    [PYTHON_BIN, "push_selected_dates.py", "--date", "2025-12-01"],
+    [PYTHON_BIN, "push_selected_dates.py", "--date", "2025-12-08"],
+    [PYTHON_BIN, "push_selected_dates.py", "--date", "2025-12-15"],
 ]
 for cmd in commands:
     print(f"Running: {' '.join(cmd)}")
