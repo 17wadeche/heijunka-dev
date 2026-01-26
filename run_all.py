b4d1b5b77d542ceb346272a49794bae9f09b014f
17wadeche <78323562+17wadeche@users.noreply.github.com>
2026-01-26 15:16:48 -0600

    add to run all

--- run_all.py
+++ run_all.py
@@ -4,6 +4,7 @@ import sys
 from datetime import date
 PYTHON_BIN = sys.executable
 commands = [
+    [PYTHON_BIN, "get_timeliness.py"],
     [PYTHON_BIN, "heijunka_new_layout.py", "--all"],
     [PYTHON_BIN, "collect_non_wip_new.py", "--config", "teams.json", "--metrics", "metrics.csv", "--all", "--out", "non_wip.csv"],
     [PYTHON_BIN, "push_selected_dates.py", "--date", "2025-10-27"],
