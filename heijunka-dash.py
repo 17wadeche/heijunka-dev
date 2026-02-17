6178c38fae9149990cc2354154c2e0b2114e3198
17wadeche <78323562+17wadeche@users.noreply.github.com>
2026-02-17 12:42:10 -0600

    push fix

--- heijunka-dash.py
+++ heijunka-dash.py
@@ -2,4 +2,3 @@ import streamlit as st
 st.set_page_config(page_title="Enterprise Dashboard", layout="wide")
 st.title("Enterprise Dashboard")
 st.info("Enterprise dashboard coming soon.")
-st.page_link("pages/Interventional_Vascular.py", label="Go to Interventional Vascular")
