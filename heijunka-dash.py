b1febc1b0e1c75db3d31e79ca58f32e0dbf9f784
17wadeche <78323562+17wadeche@users.noreply.github.com>
2026-02-17 13:01:03 -0600

    Push all

--- heijunka-dash.py
+++ heijunka-dash.py
@@ -4,10 +4,8 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
-pg = st.navigation(
-    [
-        st.Page("heijunka-dash.py", title="Enterprise"),
-        st.Page("pages/Interventional_Vascular.py", title="Interventional Vascular"),
-    ]
-)
+pg = st.navigation([
+    st.Page("pages/Enterprise.py", title="Enterprise"),
+    st.Page("pages/Interventional_Vascular.py", title="Interventional Vascular"),
+])
 pg.run()
