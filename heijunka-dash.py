9a15ecd67e1c77037adef0f52b68f97b1c19cce1
17wadeche <78323562+17wadeche@users.noreply.github.com>
2026-02-18 09:07:17 -0600

    hide strip

--- heijunka-dash.py
+++ heijunka-dash.py
@@ -1,9 +1,17 @@
 import streamlit as st
+
 st.set_page_config(
     page_title="Enterprise Dashboard",
     layout="wide",
     initial_sidebar_state="expanded",
 )
+st.markdown("""
+    <style>
+    [data-testid="stToolbar"] {
+        visibility: hidden;
+    }
+    </style>
+""", unsafe_allow_html=True)
 pg = st.navigation([
     st.Page("pages/Enterprise.py", title="Enterprise"),
     st.Page("pages/Interventional_Vascular.py", title="Interventional Vascular"),
