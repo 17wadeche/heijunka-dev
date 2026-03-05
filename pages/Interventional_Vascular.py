b679af5af2d099832c1de0a8b3ae2163d8edfdee
17wadeche <78323562+17wadeche@users.noreply.github.com>
2026-03-05 09:27:33 -0600

    push

--- pages/Interventional_Vascular.py
+++ pages/Interventional_Vascular.py
@@ -403,6 +403,7 @@ def split_nonwip_activity_minutes(cat: pd.DataFrame) -> pd.DataFrame:
             "aem":"Meeting",
             "aem cqs meting":"Meeting",
             "scrum &call": "Scrum & Action",
+            "it ticket/call": "IT Issue/Update",
             "email & iml" : "Email & IM",
             "crdn call":"Meeting",
             "audit meeting": "Audit",
