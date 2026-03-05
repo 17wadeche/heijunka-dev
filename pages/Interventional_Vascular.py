ad59aec705899658dc3d4c2c55a1023a8f2fa687
17wadeche <78323562+17wadeche@users.noreply.github.com>
2026-03-05 09:19:52 -0600

    fie

--- pages/Interventional_Vascular.py
+++ pages/Interventional_Vascular.py
@@ -397,6 +397,7 @@ def split_nonwip_activity_minutes(cat: pd.DataFrame) -> pd.DataFrame:
             "literature meeting": "Meeting",
             "literature/readcube meeting": "Meeting",
             "aging wip":"Aged WIP Review",
+            "aging file review":"Aged WIP Review",
             "report request/call": "Meeting",
             "audit meeting": "Audit",
             "scrum": "Scrum & Action",
