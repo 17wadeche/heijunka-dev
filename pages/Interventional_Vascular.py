7b0b5a25d209530777c56bc2c9ab1f8fdcb0c431
17wadeche <78323562+17wadeche@users.noreply.github.com>
2026-03-05 08:57:08 -0600

    e

--- pages/Interventional_Vascular.py
+++ pages/Interventional_Vascular.py
@@ -389,7 +389,6 @@ def split_nonwip_activity_minutes(cat: pd.DataFrame) -> pd.DataFrame:
             "risk management knowledge sharing call": "Risk Management Knowledge Sharing Call",
             "risk mangement knowledge sharing call": "Risk Management Knowledge Sharing Call",
             "risk mgmt kniwledge session": "Risk Management Knowledge Sharing Call",
-            "email": "Email & IM",
             "film meeting": "Meeting",
             "coding/risk mgmt meeting": "Meeting",
             "coding meeting": "Meeting",
@@ -409,7 +408,8 @@ def split_nonwip_activity_minutes(cat: pd.DataFrame) -> pd.DataFrame:
             "pmq query":"PMQ Meeting",
             "it update": "IT Issue/Update",
             "aortic report": "Team Report",
-            "pvh report": "Team Report"
+            "pvh report": "Team Report",
+            "email": "Email & IM"
         }
         if key in explicit_map:
             return explicit_map[key]
