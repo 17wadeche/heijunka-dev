41c74b5d93ee3da2b94210314ec61cfcaf8c731a
17wadeche <78323562+17wadeche@users.noreply.github.com>
2026-03-08 20:06:01 -0500

    fsda

--- pages/Interventional_Vascular.py
+++ pages/Interventional_Vascular.py
@@ -354,6 +354,7 @@ def split_nonwip_activity_minutes(cat: pd.DataFrame) -> pd.DataFrame:
             "metrics/scrum/schedule for tomorrow": "Metrics & Schedule",
             "metrics, tuesday schedule": "Metrics & Schedule",
             "pulled work": "Metrics & Schedule",
+            "metrics & monday's schedule": "Metrics & Schedule",
             "admin/pack up desk":"Admin",
             "imdrf":"Coding",
             "capa meeting": "CAPA",
