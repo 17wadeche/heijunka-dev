eaa5d1b608b7ee6e03ef0aa1c1b0f059d0577268
17wadeche <78323562+17wadeche@users.noreply.github.com>
2026-03-09 09:16:05 -0500

    run all

--- run_all.py
+++ run_all.py
@@ -26,6 +26,7 @@ commands = [
     [PYTHON_BIN, "push_selected_dates.py", "--date", "2026-02-16"],
     [PYTHON_BIN, "push_selected_dates.py", "--date", "2026-02-23"],
     [PYTHON_BIN, "push_selected_dates.py", "--date", "2026-03-02"],
+    [PYTHON_BIN, "push_selected_dates.py", "--date", "2026-03-09"],
     [PYTHON_BIN, "Scrape_wip_ns.py"],
     [PYTHON_BIN, "build_ns_non_wip_activities.py"],
     [PYTHON_BIN, "scrape_wip_crm.py"],
