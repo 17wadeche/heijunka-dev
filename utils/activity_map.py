a275993668066ab0cbb3a654a31340101003bcbe
17wadeche <78323562+17wadeche@users.noreply.github.com>
2026-05-11 18:30:26 -0500

    d

--- utils/activity_map.py
+++ utils/activity_map.py
@@ -463,6 +463,7 @@ ACTIVITY_MAP = {
     "review first can this be closed as duplicate of":"WIP Event/Review",
     "training new hires another": "Training/Mentoring",
     "review practice closure": "Training/Mentoring",
+    "training, scrum, collaboration": "Training/Mentoring",
     "parking permit":"Admin",
     "ehs refresher": "Training/Mentoring",
     "emails/queries/etc":"Email & IM",
