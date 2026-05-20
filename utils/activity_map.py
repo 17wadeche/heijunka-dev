ad70a831b996af6b1770b2c8f0d5a1f72ea16908
17wadeche <78323562+17wadeche@users.noreply.github.com>
2026-05-20 14:18:36 -0500

    df

--- utils/activity_map.py
+++ utils/activity_map.py
@@ -1323,6 +1323,7 @@ ACTIVITY_MAP = {
     "scrum 30": "Scrum & Action",
     "scrum/meeting": "Scrum & Action",
     "training (on-boarding, product training, etc)":"Training/Mentoring",
+    "training (on-boarding, product training, enterprise training, etc)":"Training/Mentoring",
     "blockers (it issues, gch issues, computer updates)": "IT Issue/Update",
     "scum": "Scrum & Action",
     "it update": "IT Issue/Update",
