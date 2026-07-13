c5f74d64a921abb57aff8aa9e03ece38a33bddb9
17wadeche <78323562+17wadeche@users.noreply.github.com>
2026-07-13 15:04:32 -0500

    d

--- utils/activity_map.py
+++ utils/activity_map.py
@@ -4139,6 +4139,7 @@ ACTIVITY_MAP = {
     "aortic ri report":"RI",
     "looking through wait investigations and open dhr": "WIP/Event Review",
     "reviewing files that need dhr.": "WIP/Event Review",
+    "non eu vig rr": "WIP/Event Review",
     "comm & reg report": "WIP/Event Review",
     "ot event review 4 events submitted": "WIP/Event Review",
     "completing vs dhr request forms and sending to harmac (total of 7 dhr )": "WIP/Event Review",
