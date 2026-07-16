fcd46faa3d8c1393e647e5c7d2b60ee98e7e0120
17wadeche <78323562+17wadeche@users.noreply.github.com>
2026-07-16 10:46:56 -0500

    reorder

--- run_all.py
+++ run_all.py
@@ -45,10 +45,10 @@ commands = [
     [PYTHON_BIN, "build_crm_non_wip_activities.py", "--team", "DS"],
     [PYTHON_BIN, "build_crm_non_wip_activities.py", "--team", "NI"],
     [PYTHON_BIN, "build_crm_non_wip_activities.py", "--team", "NI & PM MEIC"],
-    [PYTHON_BIN, "build_crm_non_wip_activities.py", "--team", "PM-CTS", "--weeks-back", "1"],
     [PYTHON_BIN, "scrape_wip_ms.py"], # 1:38
     [PYTHON_BIN, "build_ms_non_wip_activities.py"], # 1:44
     [PYTHON_BIN, "build_ns_non_wip_activities.py"], # 1:36
+    [PYTHON_BIN, "build_crm_non_wip_activities.py", "--team", "PM-CTS", "--weeks-back", "1"],
     [PYTHON_BIN, "build_crm_non_wip_activities.py", "--team", "MCS"],
     [PYTHON_BIN, "build_crm_non_wip_activities.py", "--team", "PM-CTS IND"],
     [PYTHON_BIN, "build_crm_non_wip_activities.py", "--team", "Lit & Letters", "--weeks-back", "1"],
