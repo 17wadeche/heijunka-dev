701ada5daec258aa840fd1d135f153ffed4c926f
17wadeche <78323562+17wadeche@users.noreply.github.com>
2026-07-25 13:31:12 -0500

    d

--- utils/activity_map.py
+++ utils/activity_map.py
@@ -3174,7 +3174,7 @@ ACTIVITY_MAP = {
     "pvh aged file review":"WIP/Event Review",
     "pending rds":"WIP/Event Review",
     "gemba and gemba prep":"Gemba",
-    "audit review complaint ":"Audit",
+    "audit review complaint file":"Audit",
     "pmq cr post-meeting q's review":"PMQ Meeting",
     "fda report":"FDA Meeting & Action",
     "sorting out hold box (pvh)":"Shipment",
